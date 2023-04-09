a91bd9d77fe4fe6d4d225b0f3dd5e5b1ea2bc42b
LeoVerto <LeoVerto@users.noreply.github.com>
2023-04-09 02:38:41 +0200

    initial commit

---
+++ key_manager.py
@@ -0,0 +1,99 @@
+import re
+import os
+import pathlib
+
+from dataclasses import dataclass
+
+
+@dataclass(unsafe_hash=True)
+class Circle:
+    label: str = ""
+    key: str = ""
+    joined: bool = False
+    betrayed: bool = False
+
+    def __post_init__(self):
+        self.label = self.label.lower()
+        self.key = self.key.lower()
+
+    def equal(self, other):
+        if other.__class__ is not self.__class__:
+            return NotImplemented
+        return (self.label, self.key) == (other.label, other.key)
+
+
+def load_command_file(file):
+    circles = set()
+    regex = re.compile(r"label:(.*)\s+key:(.*)")
+    with open(file, "r") as f:
+        for line in f:
+            m = regex.search(line)
+            if m:
+                circles.add(Circle(m.group(1), m.group(2)))
+    return circles
+
+
+def load_key_file(file):
+    circles = set()
+    regex = re.compile(r"\((.*)\)\s+\((.*)\)")
+    with open(file, "r") as f:
+        for line in f:
+            m = regex.match(line)
+            if m:
+                circles.add(Circle(m.group(1), m.group(2)))
+    return circles
+
+
+def load_joined_labels():
+    labels = []
+    with open("circles/joined.txt") as f:
+        for line in f:
+            labels.append(line.lower().strip())
+    return labels
+
+
+def get_duplicate_labels(circles):
+    labels = []
+    dupes = []
+    for circle in circles:
+        if circle.label in labels:
+            dupes.append(circle.label)
+        else:
+            labels.append(circle.label)
+    return dupes
+
+
+def set_joined(circles):
+    joined = load_joined_labels()
+    for circle in circles:
+        if circle.label in joined:
+            circle.joined = True
+    return circles
+
+
+def get_unjoined(circles):
+    unjoined = []
+    for circle in circles:
+        if not circle.joined:
+            unjoined.append(circle)
+    return unjoined
+
+
+def load_and_get_unjoined_circles():
+    circles = set()
+    for path in pathlib.Path("./circles").glob("*_commands.txt"):
+        circles |= load_command_file(path)
+
+    for path in pathlib.Path("./circles").glob("*_keys.txt"):
+        circles |= load_key_file(path)
+
+    circles = set_joined(circles)
+    print(f"Loaded {len(circles)} unique circles.")
+    unjoined = get_unjoined(circles)
+    print(f"Found {len(unjoined)} unjoined circles.")
+    print(unjoined)
+    return unjoined
+
+
+if __name__ == "__main__":
+    load_and_get_unjoined_circles()
---
+++ main.py
@@ -0,0 +1,23 @@
+import time
+
+import pyperclip
+
+from key_manager import Circle, load_and_get_unjoined_circles
+
+
+def generate_command(circle: Circle):
+    return f"/join circle label:{circle.label} key:{circle.key}"
+
+
+def paste_commands():
+    unjoined = load_and_get_unjoined_circles()
+
+    for circle in unjoined:
+        cmd = generate_command(circle)
+        pyperclip.copy(cmd)
+        print(".", end="", flush=True)
+        time.sleep(0.8)
+
+
+time.sleep(2)
+paste_commands()
